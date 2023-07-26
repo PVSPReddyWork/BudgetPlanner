6bea83116496afab88ac5cc519bdd57da0e9bce8
PVSPReddyWork <120256741+PVSPReddyWork@users.noreply.github.com>
2023-07-26 11:56:55 +0530

    Added temporary fields to the Add Expense Page

--- src/Proj/Views/Expenses/AddExpenses/AddExpensesPage.js
+++ src/Proj/Views/Expenses/AddExpenses/AddExpensesPage.js
@@ -3,69 +3,146 @@ import {
   CustomLogger,
   DateTimeManipulations,
 } from './../../../JavaScript/Modules/Helper.js';
+import { PaymentTypes, ExpenditureTypes } from './../ExpenseTypeConstants.js';
 
 const AddExpenses_Page = (parms) => {
-  const InputFields = [
-    {
-      id: 'index',
-      name: 'index',
-    },
-    {
-      id: 'dateOfPurchase',
-      name: 'dateOfPurchase',
-    },
-    {
-      id: 'nameOfPurchase',
-      name: 'nameOfPurchase',
-    },
-    {
-      id: 'expenditureType',
-      name: 'expenditureType',
-    },
-    {
-      id: 'paidBy',
-      name: 'paidBy',
-    },
-    {
-      id: 'amountSpend',
-      name: 'amountSpend',
-    },
-    {
-      id: 'details',
-      name: 'details',
-    },
-    {
-      id: 'dateCreated',
-      name: 'dateCreated',
-    },
-    {
-      id: 'expenditureId',
-      name: 'expenditureId',
-    },
-    {
-      id: 'isSynced',
-      name: 'isSynced',
-    },
-  ];
+  const NAME_OF_PURCHASE = 'nameOfPurchase';
+  const AMOUNT_SPEND = 'amountSpend';
+  const PAID_BY = 'paidBy';
+  const DATE_OF_PURCHASE = 'dateOfPurchase';
+  const EXPENDITURE_TYPE = 'expenditureType';
+  const DETAILS = 'details';
+
+  const paymentTypePickerValues = [];
+  PaymentTypes.forEach((element) => {
+    paymentTypePickerValues.push(element.displayText);
+  });
+
+  const expenditureTypePickerValues = [];
+  ExpenditureTypes.forEach((element) => {
+    expenditureTypePickerValues.push(element.displayText);
+  });
+
   const [expensesData, setExpensesData] = useState({
-    expenses: {
-      index: '',
-      dateOfPurchase: '',
-      nameOfPurchase: '',
-      expenditureType: '',
-      paidBy: '',
-      amountSpend: 0,
-      details: '',
-      dateCreated: '',
-      expenditureId: '',
-      isSynced: '',
-    },
+    expenses: [
+      {
+        key: NAME_OF_PURCHASE,
+        inputID: NAME_OF_PURCHASE,
+        placeHolder: 'Enter Name/Title of Expense',
+        legendTitle: 'Enter Name/Title of Expense',
+        hintText: '',
+        isError: false,
+        value: '',
+      },
+      {
+        key: AMOUNT_SPEND,
+        inputID: AMOUNT_SPEND,
+        placeHolder: 'Amount Spend',
+        legendTitle: 'Amount Spend',
+        hintText: '',
+        isError: false,
+        value: 0,
+        isNumber: true,
+      },
+      {
+        key: PAID_BY,
+        inputID: PAID_BY,
+        placeHolder: 'Payment Type',
+        legendTitle: 'Payment Type',
+        hintText: '',
+        isError: false,
+        value: '',
+        isPicker: true,
+        pickerData: paymentTypePickerValues,
+      },
+      {
+        key: DATE_OF_PURCHASE,
+        inputID: DATE_OF_PURCHASE,
+        placeHolder: 'Date of Purchase',
+        legendTitle: 'Date of Purchase',
+        hintText: '',
+        isError: false,
+        value: '',
+        isDatePicker: true,
+      },
+      {
+        key: EXPENDITURE_TYPE,
+        inputID: EXPENDITURE_TYPE,
+        placeHolder: 'Expenditure Type',
+        legendTitle: 'Expenditure Type',
+        hintText: '',
+        isError: false,
+        value: '',
+        isPicker: true,
+        pickerData: expenditureTypePickerValues,
+      },
+      {
+        key: DETAILS,
+        inputID: DETAILS,
+        placeHolder: 'Details',
+        legendTitle: 'Details',
+        hintText: '',
+        isError: false,
+        value: '',
+        isEditor: true,
+      },
+    ],
   });
 
   const AddFieldsUI = () => {
     try {
-      var inputFields = InputFields.map((item) => {
-        return <input type="Text" />;
+      var inputFields = expensesData.expenses.map((item) => {
+        if (item.isDatePicker) {
+          return (
+            <div>
+              <input
+                type="Text"
+                placeHolder={item.placeHolder}
+                value={item.value}
+              />
+            </div>
+          );
+        } else if (item.isNumber) {
+          return (
+            <div>
+              <input
+                type="Number"
+                placeHolder={item.placeHolder}
+                value={item.value}
+              />
+            </div>
+          );
+        } else if (item.isPicker) {
+          return (
+            <div>
+              <input
+                type="Text"
+                placeHolder={item.placeHolder}
+                value={item.value}
+              />
+            </div>
+          );
+        } else if (item.isEditor) {
+          return (
+            <div>
+              <input
+                type="Text"
+                placeHolder={item.placeHolder}
+                value={item.value}
+              />
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <input
+                type="Text"
+                placeHolder={item.placeHolder}
+                value={item.value}
+              />
+            </div>
+          );
+        }
       });
       return <>{inputFields}</>;
     } catch (ex) {
