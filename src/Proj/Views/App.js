273796cfdda3edb80fa22b1fe232ff270afb03cf
PVSPReddyWork <120256741+PVSPReddyWork@users.noreply.github.com>
2023-07-27 12:13:48 +0530

    Adding error boundaries back to project

--- src/Proj/Views/App.js
+++ src/Proj/Views/App.js
@@ -4,16 +4,17 @@ import CustomNavigation from './NavigationPage/NavigationPage.js';
 import ErrorBoundary from './ErrorDisplayHandler/ErrorBoundaryPage.js';
 
 export default function App() {
+  /** /
   return (
     <div>
       <CustomNavigation />
     </div>
-  ); /*
+  );/**/ /**/
   return (
     <div>
       <ErrorBoundary fallback={<p>Something went wrong</p>}>
         <CustomNavigation />
       </ErrorBoundary>
     </div>
-  );*/
+  ); /**/
 }
