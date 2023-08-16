ee725f52456c867770e18b0a2161772598e2d39c
PVSPReddyWork <120256741+PVSPReddyWork@users.noreply.github.com>
2023-08-16 14:00:21 +0530

    Added useEfect and state to the bar graph svg

--- src/Proj/JavaScript/Modules/GraphMaker/BarSVGChart.js
+++ src/Proj/JavaScript/Modules/GraphMaker/BarSVGChart.js
@@ -1,17 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ChartStyles.css';
 
 const BarSVGChart = (params) => {
-  const { x_Data, y_Data } = params;
+  const { xData, yData } = params;
 
-  const xData = ['Street Food', 'Fun and Entertainment', 'Recharge', 'Rent'];
-  const yData = ['23453', '34234.34', '23424.22', '1221.21'];
+  const defaultData = {
+    xData: [],
+    yData: [],
+    xSpaceRatio: 0,
+    ySpaceRatio: 0,
+    textSizeScale: 0.3,
+  };
+  const [chartData, setChartData] = useState({ data: defaultData });
 
-  const xSpaceRatio = 80 / (xData.length + 1);
+  useEffect(() => {
+    initGraph();
+  }, []);
 
-  const ySpaceRatio = 80 / (yData.length + 1);
+  const initGraph = () => {
+    try {
+      const x_Data = [
+        'Street Food',
+        'Fun and Entertainment',
+        'Recharge',
+        'Rent',
+      ];
+      let y_Data = ['23453', '34234.34', '23424.22', '1221.21'];
 
-  const textSizeScale = 0.3;
+      y_Data = y_Data.sort(function (a, b) {
+        return b - a;
+      });
+
+      y_Data.forEach((item) => {
+        item = item.includes('.') ? item : `${item}.00`;
+      });
+
+      const x_SpaceRatio = 80 / (x_Data.length + 1);
+      const y_SpaceRatio = 80 / (y_Data.length + 1);
+      const updatedData = {
+        ...chartData.data,
+        xData: x_Data,
+        yData: y_Data,
+        xSpaceRatio: x_SpaceRatio,
+        ySpaceRatio: y_SpaceRatio,
+      };
+      setChartData({ ...chartData, data: updatedData });
+    } catch (ex) {}
+  };
+  /*
+  useEffect(() => {
+    initGraphData();
+  }, []);
+
+  useEffect(() => {
+    updateGraph();
+  }, [chartData.data.yData]);
+
+  const initGraphData = () => {
+    try {
+      const x_Data = [
+        'Street Food',
+        'Fun and Entertainment',
+        'Recharge',
+        'Rent',
+      ];
+      let y_Data = ['23453', '34234.34', '23424.22', '1221.21'];
+
+      y_Data = y_Data.sort(function (a, b) {
+        return b - a;
+      });
+
+      y_Data.forEach((item) => {
+        item = item.includes('.') ? item : `${item}.00`;
+      });
+
+      const updatedData = {
+        ...chartData.data,
+        xData: x_Data,
+        yData: y_Data,
+      };
+      setChartData({ ...chartData, data: updatedData });
+    } catch (ex) {}
+  };
+
+  const updateGraph = () => {
+    try {
+      const x_SpaceRatio = 80 / (chartData.data.xData.length + 1);
+      const y_SpaceRatio = 80 / (chartData.data.yData.length + 1);
+      const updatedData = {
+        ...chartData.data,
+        xSpaceRatio: x_SpaceRatio,
+        ySpaceRatio: y_SpaceRatio,
+      };
+      setChartData({ ...chartData, data: updatedData });
+    } catch (ex) {}
+  };
+  */
 
   return (
     <>
@@ -25,15 +109,17 @@ const BarSVGChart = (params) => {
 
           {/* This is for y-axis info */}
           <g>
-            {yData.map((item, index) => {
-              let yIndex = ySpaceRatio * (index + 1);
+            {chartData.data.yData.map((item, index) => {
+              let yIndex = chartData.data.ySpaceRatio * (index + 1);
               return (
                 <text
                   x={0}
                   y={0}
                   fill="red"
-                  text-anchor="end"
-                  transform={`translate(${20},${yIndex}) rotate(${-30} ${0},${0}) scale(${textSizeScale})`}
+                  textAnchor="end"
+                  transform={`translate(${20},${yIndex}) rotate(${-30} ${0},${0}) scale(${
+                    chartData.data.textSizeScale
+                  })`}
                 >
                   {item}
                 </text>
@@ -43,13 +129,13 @@ const BarSVGChart = (params) => {
 
           {/* This is for x-axis info */}
           <g>
-            {xData.map((item, index) => {
+            {chartData.data.xData.map((item, index) => {
               var multiText = null;
               if (item.includes(' ')) {
                 multiText = item.split(' ');
               }
 
-              let xIndex = xSpaceRatio * (index + 1);
+              let xIndex = chartData.data.xSpaceRatio * (index + 1);
 
               if (multiText !== null && multiText !== undefined) {
                 if (multiText.length > 2) {
@@ -65,8 +151,10 @@ const BarSVGChart = (params) => {
                   x={0}
                   y={0}
                   fill="red"
-                  text-anchor="end"
-                  transform={`translate(${xIndex},${84}) rotate(${320} ${0},${0}) scale(${textSizeScale})`}
+                  textAnchor="end"
+                  transform={`translate(${xIndex},${84}) rotate(${320} ${0},${0}) scale(${
+                    chartData.data.textSizeScale
+                  })`}
                 >
                   {multiText !== null && multiText !== undefined
                     ? multiText.map((multiTextItem, multiTextIndex) => {
@@ -79,7 +167,9 @@ const BarSVGChart = (params) => {
                             text-anchor="end"
                             transform={`translate(${
                               xIndex + multiTextIndex * 10
-                            },${84}) rotate(${320} ${0},${0}) scale(${textSizeScale})`}
+                            },${84}) rotate(${320} ${0},${0}) scale(${
+                              chartData.data.textSizeScale
+                            })`}
                           >
                             {multiTextItem}
                           </tspan>
@@ -92,7 +182,7 @@ const BarSVGChart = (params) => {
           </g>
 
           {/* This is for graph data bars */}
-          <g>{yData.map((item, index) => {})}</g>
+          <g>{chartData.data.yData.map((item, index) => {})}</g>
         </svg>
       </div>
     </>
